ebd44c4cde4bce96697627de7450f2e1da8d0d68
Nicholas Maundu <nickiemaundu19@gmail.com>
2021-12-09 00:25:43 +0300

    Day 7: accessing views through mBinding and onBackPressed navigation

--- app/src/main/java/com/nicholas/mileagechecker/view/activities/AddCar.kt
+++ app/src/main/java/com/nicholas/mileagechecker/view/activities/AddCar.kt
@@ -2,9 +2,12 @@ package com.nicholas.mileagechecker.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.nicholas.mileagechecker.R
 import com.nicholas.mileagechecker.databinding.ActivityAddCarBinding
 
-class AddCar : AppCompatActivity() {
+class AddCar : AppCompatActivity() , View.OnClickListener{
 //    Not when you use viewBinding feature, each xml file generates a
 //    binding class that allows access to all html elements eg. result_profile-
 //    generates a binding class ResultingProfileBinding
@@ -14,5 +17,32 @@ class AddCar : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = ActivityAddCarBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        //to the actionBar you have to call the method here
+        setUpActionBar()
+        //the onclick method(assigning our onclick listener the view)
+        mBinding.addCarPhotos.setOnClickListener(this)
+    }
+    //set up action bar
+    private fun setUpActionBar(){
+        setSupportActionBar(mBinding.addCarToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.addCarToolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {//null check makes sure that the view is not null
+            when(v.id){
+                    R.id.addCarPhotos->{
+                        Toast.makeText(this, "car added", Toast.LENGTH_SHORT)
+                            .show()
+                        return
+
+                    }
+            }
+        }
     }
 }
