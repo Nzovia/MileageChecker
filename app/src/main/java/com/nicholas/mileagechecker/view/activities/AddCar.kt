534b463ab0de9547a793272654d24061addcf9b9
Nicholas Maundu <nickiemaundu19@gmail.com>
2021-12-12 13:12:26 +0300

    Day 7: added actions on the custom dialog

--- app/src/main/java/com/nicholas/mileagechecker/view/activities/AddCar.kt
+++ app/src/main/java/com/nicholas/mileagechecker/view/activities/AddCar.kt
@@ -1,11 +1,13 @@
 package com.nicholas.mileagechecker.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.nicholas.mileagechecker.R
 import com.nicholas.mileagechecker.databinding.ActivityAddCarBinding
+import com.nicholas.mileagechecker.databinding.CustomphotodialogBinding
 
 class AddCar : AppCompatActivity() , View.OnClickListener{
 //    Not when you use viewBinding feature, each xml file generates a
@@ -37,8 +39,9 @@ class AddCar : AppCompatActivity() , View.OnClickListener{
         if (v != null) {//null check makes sure that the view is not null
             when(v.id){
                     R.id.addCarPhotos->{
-                        Toast.makeText(this, "car added", Toast.LENGTH_SHORT)
-                            .show()
+//                        Toast.makeText(this, "car added", Toast.LENGTH_SHORT)
+//                            .show()
+                        showCustomDialog()
                         return
 
                     }
@@ -46,6 +49,21 @@ class AddCar : AppCompatActivity() , View.OnClickListener{
         }
     }
     private fun showCustomDialog(){
+        val dialog = Dialog(this)
+        val binding : CustomphotodialogBinding = CustomphotodialogBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+        binding.tvCamera.setOnClickListener(){
+            Toast.makeText(this, "Camera Clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        binding.tvGallery.setOnClickListener(){
+            Toast.makeText(this,"gallery clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        //for the dialog to appear
+        dialog.show()
 
     }
 }
